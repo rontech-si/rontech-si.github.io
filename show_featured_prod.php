cd0b6561840f0a55ce182c08a322dbb1e2ac1657
Toydotgam <MatthewJPersonalAcct@outlook.com>
2020-11-20 11:45:07 +1000

    HTML Re-write for gh-pages Compatability
    This makes the contact form obselete for sure. I doubt it would have worked anyway...

--- show_featured_prod.php
+++
@@ -1,8 +0,0 @@
-<table border="0" cellspacing="0" cellpadding="0">
-  <tr>
-	<td width="52" align="left" valign="top" style="padding-right:10px; padding-left:10px;"><a href="rontech_security_grade_a1_monitoring.php" title="Grade A1 Monitoring Station"><img src="images/ajx_dv7380301.gif" alt="Grade A1 Monitoring Station" border="0" title="Grade A1 Monitoring Station"/></a></td>
-	<td width="183" align="left" valign="top"><h2>Grade A1 Monitoring Station</h2>
-	  <span class="h2_txt_black">The most effective way to reduce loss and protect your assets is to have your security system monitored from a...</span><br />
-	<div align="right"><a href="rontech_security_grade_a1_monitoring.php" class="send_enquiry" title="More">more >></a></div></td>
-  </tr>
-</table>
